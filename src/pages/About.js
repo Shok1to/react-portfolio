cbb3807185aec5c8c72df303b4306dc70e62806d
Shok1to <shoko.takahashi.911@gmail.com>
2023-08-02 21:38:42 -0400

    added contact page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import '../styles/About.css'; 
+import '../styles/About.css';
+import Photo from '../assets/IMG_7491.JPG'
 export default function About() {
   return (
     <div className="home">
-      <div className="about">
-        <h2>Hello,I'm Shoko</h2>
-        <div className="prompt">
-          <p>A Front-End Developer based in Toronto. I love creating user-friendly websites that look great. 
-            I'm currently learning at the University of Toronto boot camp to improve my skills and stay updated with the latest web development trends. 
-            Let's build something awesome together!</p>
-          
+
+      <div className="about-cont">
+
+        <div className="about">
+          <h2>Hello, I'm Shoko</h2>
+          <div className="prompt">
+            <p>A Front-End Developer based in Toronto. I love creating user-friendly websites that look great.
+              I'm currently learning at the University of Toronto boot camp to improve my skills and stay updated with the latest web development trends.
+              Let's build something awesome together!</p>
+          </div>
         </div>
+        <div className='profile-container'>
+         <img className="profile" src={Photo} alt='profile photo' />
+        </div>
+        
       </div>
       <div className="skills"></div>
     </div>
 
- );
+  );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact() {
+
+  const [formState, setFormState]=useState({
+    name:'', 
+    email:'',
+    message:'',
+  })
+
+  const [ name, email, message] = formState;
+
+  // handleSubmit function -> not much 
+
+  // ** handleChange function -> check to make sure contents of field are valid
+
+  //  ** onBlur attributes to input and textarea elements onBlur = {handleChange}
+
   return (
     <div>
       <h1>Contact</h1>
@@ -10,6 +25,26 @@ export default function Contact() {
 
 Let's connect and create something amazing together!
       </p>
+
+      <form className='contact'>
+        <div className="form-field">
+          <label for="name">name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="form-field">
+          <label for="email">email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-field">
+          <label for="message">message:</label>
+          <textarea rows="4" id="message" name="message" required />
+        </div>
+        <button type="submit">submit</button>
+      </form>
+
+
+
+
     </div>
   );
 }
