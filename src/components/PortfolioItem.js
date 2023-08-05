a28806436837e73f64bf06a3342da81f7c40ad60
Shok1to <shoko.takahashi.911@gmail.com>
2023-08-04 23:47:44 -0400

    fixed portfolioDisplay

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -4,17 +4,15 @@ import{ useNavigate } from "react-router-dom";
 export default function PortfolioItem({ image, name, url }) {
   const navigate = useNavigate();
   return (
-    <div 
+    <a 
       className='portfolioItem'
-      onClick={() =>{
-        navigate(`${url}`);
-      }}
+      href={url}
     >
      <div style={{ backgroundImage: `url(${image})` }} className='bgImage' />
      <h1>{name}</h1>
       
         
      
-    </div>
+    </a>
   );
 }
