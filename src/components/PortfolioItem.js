0b43ad89b0e28ad296330d523e044361d95c54c6
Shok1to <shoko.takahashi.911@gmail.com>
2023-08-06 00:09:16 -0400

    added github link

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import{ useNavigate } from "react-router-dom";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
-export default function PortfolioItem({ image, name, url }) {
+export default function PortfolioItem({ image, name, url, gitRepo }) {
   const navigate = useNavigate();
   return (
     <a 
@@ -11,7 +11,7 @@ export default function PortfolioItem({ image, name, url }) {
     >
      <div style={{ backgroundImage: `url(${image})` }} className='bgImage' />
      <h1>{name}</h1>
-     <a href={gitRepo}><GitHubIcon /></a>
+     <a href={ gitRepo }><GitHubIcon /></a>
       
         
      
