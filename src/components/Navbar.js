8e3b9affedd3278b5ec564c968f555fb940dfb10
Shok1to <shoko.takahashi.911@gmail.com>
2023-08-07 20:45:14 -0400

    added active link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { NavLink, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../styles/Navbar.css'; 
 
-function Navbar() {
+function Navbar({ currentPage, handleChangePage }) {
   return (
     <div className="navbar">
       <div className="toggleButton">
         <button></button>
       </div>
       <div className="Links">
-        <NavLink to="/react-portfolio/About" activeClassName="active">About</NavLink>
-        <NavLink to="/react-portfolio/Portfolio" activeClassName="active">Portfolio</NavLink>
-        <NavLink to="/react-portfolio/Resume" activeClassName="active">Resume</NavLink>
-        <NavLink to="/react-portfolio/Contact" activeClassName="active">Contact</NavLink>
+        <Link to="/react-portfolio/About" onClick={() => handleChangePage("About")}
+            className={currentPage === "About" ? 'nav-link active' : 'nav-link'}>About</Link>
+            
+        <Link to="/react-portfolio/Portfolio" onClick={() => handleChangePage("Portfolio")}
+            className={currentPage === "Portfolio" ? 'nav-link active' : 'nav-link'}>Portfolio</Link>
+
+        <Link to="/react-portfolio/Resume" onClick={() => handleChangePage("Resume")}
+            className={currentPage === "Resume" ? 'nav-link active' : 'nav-link'}>Resume</Link>
+
+        <Link to="/react-portfolio/Contact" onClick={() => handleChangePage("Contact")}
+            className={currentPage === "Contact" ? 'nav-link active' : 'nav-link'}>Contact</Link>
       </div>
     </div>
   );
